a653a15c4a000f9786f35d25d8c65d983d090355
Kamil Kowalczyk <kkowalczyk@clearcoursedev.com>
2024-03-14 13:11:06 +0100

    section 8

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -1,15 +1,16 @@
 
 import ReactDOM from 'react-dom/client'
-import App from './app/layout/App'
 import 'semantic-ui-css/semantic.min.css'
 import './app/layout/styles.css'
 import React from 'react'
-import { StoreContext, store } from './app/layout/stores/store'
+import { StoreContext, store } from './app/api/stores/store'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './app/router/Routes'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
     <StoreContext.Provider value={store}>
-    <App />
+     <RouterProvider router={router}/>
     </StoreContext.Provider>
     </React.StrictMode>
 )
